c317ccadb9f711d1df1f372a39511c7cd0bc1517
Dileep <dileepkumar919996@gmail.com>
2023-01-10 00:20:05 +0530

    intern

--- src/index.js
+++ src/index.js
@@ -3,25 +3,25 @@ const express = require("express");
 const route = require("./routes/route");
 
 const bodyParser = require('body-parser')
-const {default:mongoose}=require('mongoose')
+const { default: mongoose } = require('mongoose')
 
-const app=express();
+const app = express();
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-mongoose.connect("mongodb+srv://devendra_29:I28Cx63EjuXQjHtQ@devendra.ytysqub.mongodb.net/intern", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
-
-
-app.use('/',route)
+mongoose.connect("mongodb+srv://dileepkm:L3cuCdGwQQWTF3Hs@cluster0.iqkms8u.mongodb.net/intern", {
+        useNewUrlParser: true
+    })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 
-app.listen(process.env.PORT || 3001, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3001))
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
