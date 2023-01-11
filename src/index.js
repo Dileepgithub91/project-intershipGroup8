7593526df4d1834414f2e573c464d4b81c60cadb
Dileep <dileepkumar919996@gmail.com>
2023-01-11 23:29:12 +0530

    project2 final

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ const route = require("./routes/route");
 
 const mongoose = require('mongoose')
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://devendra_29:I28Cx63EjuXQjHtQ@devendra.ytysqub.mongodb.net/intern")
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+mongoose.connect("mongodb+srv://dileepkm:L3cuCdGwQQWTF3Hs@cluster0.iqkms8u.mongodb.net/test")
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 
 
-app.use('/',route)
+app.use('/', route)
 
 
-app.listen(3001, function () {
-    console.log('Express app running on port ' +  3001)
+app.listen(3000, function() {
+    console.log('Express app running on port ' + 3000)
 });
